1585cc35065ac137cd4314812e5a974856855773
Hamid <hamid24@navgurukul.org>
2026-07-05 18:49:17 +0530

    change

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -4,13 +4,19 @@ import { Header } from '../../components/Header';
 import { ProductsGrid } from './ProductsGrid';
 import './HomePage.css';
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const getHomeData = async () => {
-      const response = await axios.get('/api/products');
-      setProducts(response.data);
+      try {
+        const response = await axios.get(`${API_URL}/api/products`);
+        setProducts(response.data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
     };
 
     getHomeData();
