7144e1dcf4c674f2e0f582bcfbe2edc8487bbbdc
Hamid <hamid24@navgurukul.org>
2026-07-06 01:50:36 +0530

    Add full stack ecommerce project

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -4,19 +4,13 @@ import { Header } from '../../components/Header';
 import { ProductsGrid } from './ProductsGrid';
 import './HomePage.css';
 
-const API_URL = import.meta.env.VITE_API_URL;
-
 export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const getHomeData = async () => {
-      try {
-        const response = await axios.get(`${API_URL}/api/products`);
-        setProducts(response.data);
-      } catch (error) {
-        console.error('Error fetching products:', error);
-      }
+      const response = await axios.get('/api/products');
+      setProducts(response.data);
     };
 
     getHomeData();
